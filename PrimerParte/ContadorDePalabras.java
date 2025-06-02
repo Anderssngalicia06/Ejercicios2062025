85cdd5f85bb3e62542def6c886ba23ff454d91df
Anderssngalicia06 <andyssjrmz.18@gmail.com>
2025-06-02 12:22:06 -0600

    Se crearon las demas clases

---
+++ PrimerParte/ContadorDePalabras.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ContadorDePalabras {
+
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String textoIngresado;
+        int totalPalabras;
+        System.out.println("Contador de Palabras");
+        System.out.println("=====================================");
+        System.out.print("Escribe una frase: ");
+        textoIngresado = scanner.nextLine();
+
+        totalPalabras = contarCantidadDePalabras(textoIngresado);
+
+        System.out.println("La cantidad de palabras es: " + totalPalabras);
+
+        scanner.close();
+    }
+
+    public static int contarCantidadDePalabras(String texto) {
+        texto = texto.trim();
+
+        if (texto.isEmpty()) {
+            return 0;
+        }
+
+        String[] listaDePalabras = texto.split("\\s+");
+
+        return listaDePalabras.length;
+    }
+
+    
+}
--- PrimerParte/EliminarUltimaPalabra.java
+++ PrimerParte/EliminarUltimaPalabra.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EliminarUltimaPalabar {
+public class EliminarUltimaPalabra {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
